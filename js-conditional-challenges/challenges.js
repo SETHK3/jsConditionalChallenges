a2f245d5c498ab4177b7d2128d73a1a5730f2e74
SETHK3 <134010843+SETHK3@users.noreply.github.com>
2023-10-23 11:00:10 -0600

    Add files via upload

---
+++ js-conditional-challenges/challenges.js
@@ -0,0 +1,51 @@
+//CHALLENGE 1
+
+// const firstObject = {
+//   id: 1,
+//   name: "Seth",
+// };
+
+// const secondObject = {
+//   address: "123 Fake St",
+//   phone: "555-555-5555",
+// };
+
+// const thirdObject = Object.assign(firstObject, secondObject);
+
+// console.log(thirdObject);
+
+//CHALLENGE 2
+
+// function largestNum(num1, num2) {
+//   return num1 > num2 ? num1 : num2;
+// }
+
+// console.log(largestNum(12, 9));
+
+//CHALLENGE 3
+
+// function firstLetter(string) {
+//   const words = string.split(" ");
+//   const firstLetterUpper = words.map(function (word) {
+//     return word.charAt(0).toUpperCase() + word.slice(1);
+//   });
+//   const firstLetterString = firstLetterUpper.join(" ");
+//   return firstLetterString;
+// }
+
+// const inputString = "i am a sentence";
+// const firstLetterString = firstLetter(inputString);
+// console.log(firstLetterString);
+
+//CHALLENGE 4
+
+// const input = "Hi there";
+
+// function sortLetters(input) {
+//   const letters = input.toUpperCase();
+//   const sortedString = letters.split("").sort().join("");
+
+//   return sortedString;
+// }
+
+// console.log(sortLetters(input));
